47ca9cea7c2efa49b2c59bd67588662c3ad79cb9
Christopher Pope <christopher@thepopes.org>
2023-02-25 12:46:13 -0500

    Completed new workout cal

--- TatterFitness.Mobile/ViewModels/History/EventCalendar/WorkoutCalendarViewModel.cs
+++ TatterFitness.Mobile/ViewModels/History/EventCalendar/WorkoutCalendarViewModel.cs
@@ -4,12 +4,28 @@ using Syncfusion.Maui.Scheduler;
 using System.Collections.ObjectModel;
 using TatterFitness.App.Interfaces.Services;
 using TatterFitness.App.Interfaces.Services.API;
+using TatterFitness.App.Views.Workouts;
 
 namespace TatterFitness.App.ViewModels.History.EventCalendar
 {
     public partial class WorkoutCalendarViewModel : ViewModelBase
     {
         private readonly IHistoriesApiService historyApi;
+        private Dictionary<string, Brush> workoutNameToBrush = new();
+        private int workoutBrushIdx = 0;
+        private List<Brush> workoutBrushes = new()
+        {
+            new SolidColorBrush(Color.FromArgb("#FF8B1FA9")),
+            new SolidColorBrush(Color.FromArgb("#FFD20100")),
+            new SolidColorBrush(Color.FromArgb("#FFFC571D")),
+            new SolidColorBrush(Color.FromArgb("#FF36B37B")),
+            new SolidColorBrush(Color.FromArgb("#FF3D4FB5")),
+            new SolidColorBrush(Color.FromArgb("#FFE47C73")),
+            new SolidColorBrush(Color.FromArgb("#FF636363")),
+            new SolidColorBrush(Color.FromArgb("#FF85461E")),
+            new SolidColorBrush(Color.FromArgb("#FF0F8644")),
+            new SolidColorBrush(Color.FromArgb("#FF01A1EF"))
+        };
 
         [ObservableProperty]
         private DateTime minDateTime = DateTime.Now;
@@ -40,6 +56,20 @@ namespace TatterFitness.App.ViewModels.History.EventCalendar
             IsBusy = false;
         }
 
+        [RelayCommand]
+        private async Task WorkoutTapped(SchedulerTappedEventArgs args)
+        {
+            if (args == null || !args.Appointments.Any())
+            {
+                return;
+            }
+
+            var workout = args.Appointments.First() as SchedulerAppointment;
+            var navData = new NavData.WorkoutSnapshotNavData(Convert.ToInt32(workout.Id));
+            await Shell.Current.GoToAsync(nameof(WorkoutSnapshotView), true, navData.ToNavDataDictionary());
+        }
+
+
         //protected override async Task ProcessDayClicked(int eventId)
         //{
         //    var navData = new NavData.WorkoutSnapshotNavData(eventId);
@@ -56,17 +86,18 @@ namespace TatterFitness.App.ViewModels.History.EventCalendar
 
             IsBusy = true;
             var dates = eventArgs.VisibleDates.OrderBy(d => d).ToList();
-            var workouts = await historyApi.ReadWorkoutEvents(dates.First(), dates.Last());
+            var workouts = await historyApi.ReadWorkouts(dates.First(), dates.Last());
             var workoutEvents = new ObservableCollection<SchedulerAppointment>();
             foreach (var workout in workouts)
             {
                 var ev = new SchedulerAppointment
                 {
                     IsAllDay = true,
-                    StartTime = workout.EventDate,
-                    EndTime = workout.EventDate.AddHours(1),
-                    Subject = "Workout",
-                    Background = new SolidColorBrush(Color.FromArgb("#FFFC571D"))
+                    StartTime = workout.Date,
+                    EndTime = workout.Date.AddHours(1),
+                    Subject = workout.Name,
+                    Id = workout.Id,
+                    Background = GetWorkoutBrush(workout.Name)
                 };
 
                 workoutEvents.Add(ev);
@@ -74,6 +105,20 @@ namespace TatterFitness.App.ViewModels.History.EventCalendar
 
             Workouts = workoutEvents;
             IsBusy = false;
+        }
+
+        private Brush GetWorkoutBrush(string workoutName)
+        {
+            if (!workoutNameToBrush.ContainsKey(workoutName))
+            {
+                workoutNameToBrush.Add(workoutName, workoutBrushes[workoutBrushIdx++]);
+                if (workoutBrushIdx == workoutBrushes.Count)
+                {
+                    workoutBrushIdx = 0;
+                }
+            }
+
+            return workoutNameToBrush[workoutName];
         }
     }
 }
