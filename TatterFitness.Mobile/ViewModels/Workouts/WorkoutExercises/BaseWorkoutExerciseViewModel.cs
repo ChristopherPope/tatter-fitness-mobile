28a761fde7a04062e934c6b3c17a8ee3068475ef
Christopher Pope <christopher@thepopes.org>
2023-03-10 16:43:39 -0500

    Added the MetricUpdated() command and removed CompletedSetMetricsChangedMessage handling

--- TatterFitness.Mobile/ViewModels/Workouts/WorkoutExercises/BaseWorkoutExerciseViewModel.cs
+++ TatterFitness.Mobile/ViewModels/Workouts/WorkoutExercises/BaseWorkoutExerciseViewModel.cs
@@ -16,14 +16,12 @@ using TatterFitness.Mobile.Views.History;
 using TatterFitness.Models.Enums;
 using TatterFitness.Models.Exercises;
 using TatterFitness.Models.Workouts;
-using static Android.Icu.Text.Transliterator;
 
 namespace TatterFitness.Mobile.ViewModels.Workouts.WorkoutExercises
 {
     public abstract partial class BaseWorkoutExerciseViewModel<T> :
         ViewModelBase,
-        IQueryAttributable,
-        IRecipient<CompletedSetMetricsChangedMessage>
+        IQueryAttributable
         where T : BaseSetViewModel
     {
         private readonly IWorkoutExercisesApiService workoutExercisesApi;
@@ -75,8 +73,6 @@ namespace TatterFitness.Mobile.ViewModels.Workouts.WorkoutExercises
             this.mapper = mapper;
             this.modsSelectorModal = modsSelectorModal;
             this.totalEffort = totalEffort;
-
-            WeakReferenceMessenger.Default.Register(this);
         }
 
         public void ApplyQueryAttributes(IDictionary<string, object> query)
@@ -382,14 +378,13 @@ namespace TatterFitness.Mobile.ViewModels.Workouts.WorkoutExercises
                 (currentPosition == 0 || SetVms[currentPosition - 1].IsCompleted));
         }
 
-        public void Receive(CompletedSetMetricsChangedMessage message)
+        [RelayCommand]
+        private Task MetricUpdated(int setId)
         {
-            var setId = message.Value;
-            logger.Info($"Received CompletedSetMetricsChangedMessage - {setId}");
             var setVm = SetVms.FirstOrDefault(vm => vm.Set.Id == setId);
             if (setVm == null || setId == 0)
             {
-                return;
+                return Task.CompletedTask;
             }
             var set = setVm.Set;
 
@@ -402,6 +397,8 @@ namespace TatterFitness.Mobile.ViewModels.Workouts.WorkoutExercises
             mapper.Map(updatedSet, set);
             set.ExerciseType = exerciseType;
             TotalEffort.ShowTotalEffort(WorkoutExercise.Sets);
+
+            return Task.CompletedTask;
         }
     }
 }
